1bfe8a5a15413a5379bc0b515bfc5bb4e919b6e6
charivna <124678194+charivna@users.noreply.github.com>
2023-06-30 21:46:31 +0200

    second

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,7 @@
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-
-// const iframe = document.querySelector('iframe');
-
-// const options = {
-//         id: 59777392,
-//         width: 640,
-//         loop: true
-//     };
-// const player = new Player(iframe, options);
-
-
-// const onPlay = function (currentTime) {
-//      // data is an object containing properties specific to that event
-//     const currentSec = currentTime.seconds;
-   
-//     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentSec));
-//     console.log(localStorage.getItem("videoplayer-current-time"))   
-// };
-
-// player.on("timeupdate", throttle(onPlay, 1000));
-
-// player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
-//     // seconds = JSON.parse(localStorage.getItem("videoplayer-current-time"))
-    
-
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle')
+
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
 
@@ -50,19 +11,21 @@ const player = new Player(iframe)
 const onPlay = function ({seconds}) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
 };
-player.on('timeupdate', onPlay)
+player.on('timeupdate',throttle (onPlay, 1000))
+
 const LS = localStorage.getItem("videoplayer-current-time")
 
 player.setCurrentTime(LS).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
+   
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
+            
             break;
 
         default:
-            // some other error occurred
+         
             break;
     }
 });
+
