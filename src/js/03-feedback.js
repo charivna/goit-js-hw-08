74591c9bb0f12f59c1479f561784c364f7065c3a
charivna <124678194+charivna@users.noreply.github.com>
2023-06-30 21:32:13 +0200

    video

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,13 @@
 const throttle = require('lodash.throttle')
-console.log(throttle);
+const form = document.querySelector('form')
+
+form.addEventListener('input', onInput)
+const data = {}
+function onInput() {
+    data[evt.target.name] = evt.target.value;
+    data[evt.target.email] = evt.target.value;
+
+    localStorage.setItem('')
+}
+
+console.log('hello');
