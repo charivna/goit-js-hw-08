9849f2ec64d9a523b0fc33c052a70acbadfe4a5e
charivna <124678194+charivna@users.noreply.github.com>
2023-07-01 13:05:09 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,9 +5,6 @@ const throttle = require('lodash.throttle')
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
 
-
-
-
 const onPlay = function ({seconds}) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
 };
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,89 @@
 const throttle = require('lodash.throttle')
+
 const form = document.querySelector('form')
+const button = document.querySelector('button')
+const mail = document.querySelector('input')
+const text = document.querySelector('textarea')
 
-form.addEventListener('input', onInput)
-const data = {}
-function onInput() {
+let data = {}
+
+form.addEventListener('input', throttle(onInput, 500))
+button.addEventListener('submit', onSubmit)
+localStorageHistory()
+
+
+function onInput(evt) {
     data[evt.target.name] = evt.target.value;
-    data[evt.target.email] = evt.target.value;
-
-    localStorage.setItem('')
+    localStorage.setItem("feedback-form-state", JSON.stringify(data))
 }
 
-console.log('hello');
+function onSubmit(evt) {
+    evt.preventDefault();
+        if(!data.email || !data.message){
+        alert('Заповніть форму повністю, будь-ласка.')
+    } else {
+        console.log(data);     
+        evt.target.reset();
+        localStorage.removeItem("feedback-form-state");
+        data = {};       
+    }    
+};
+
+function localStorageHistory() {
+    const storageGetItem = localStorage.getItem("feedback-form-state");
+    if (storageGetItem) {
+        data = JSON.parse(storageGetItem);
+        mail.value = data.email ?? '';
+        text.value = data.message ?? '';
+    }
+}
+
+
+
+
+
+
+
+
+
+// const STORAGE_KEY = "feedback-form-state";
+// let obj = {};
+
+// const refs = {
+//     feedbackForm: document.querySelector('.feedback-form'),
+//     inputEmail: document.querySelector('.feedback-form input'),
+//     textarea: document.querySelector('.feedback-form textarea')
+// };
+
+// refs.feedbackForm.addEventListener('submit', onSubmitForm);
+// refs.feedbackForm.addEventListener('input', throttle(onInputForm, 500));
+// localStorageHistory();
+
+// function onInputForm(event) {    
+//     obj[event.target.name] = event.target.value;
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(obj));    
+//  }
+
+// function onSubmitForm(event) {
+//     event.preventDefault();
+   
+//     if(!obj.email || !obj.message){
+//         alert('Заполните все поля, пожалуйста) 🍀')
+//     } else {
+//         console.log(obj);     
+//          event.target.reset();
+//         localStorage.removeItem(STORAGE_KEY);
+//         obj = {};       
+//     }    
+// };
+
+// function localStorageHistory() {
+//     const storageGetItem = localStorage.getItem(STORAGE_KEY);
+  
+//     if (storageGetItem) {
+//         obj = JSON.parse(storageGetItem);
+        
+//         refs.textarea.value = obj.message ?? '';
+//         refs.inputEmail.value = obj.email ?? '';
+//     }
+// }
